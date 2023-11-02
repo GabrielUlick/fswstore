a0c889ab11f0917688b1c754e3ea2dc1ffd56f15
Gabriel Ulick <gabrieludb@hotmail.com>
2023-11-02 17:34:46 -0300

    feat: add second banner home

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -28,8 +28,19 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
+        <p className="font-bold pl-5 uppercase mb-3">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src={"/banner-home-02.png"}
+        height={0}
+        width={0}
+        className="h-auto w-full px-5"
+        sizes="100vw"
+        alt="Até 55% de desconto em mouses!"
+      />
+
     </div>
   );
 }
