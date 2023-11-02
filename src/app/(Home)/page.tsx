8f45094464db61f71474bfe19903720d9f8a55ec
Gabriel Ulick <gabrieludb@hotmail.com>
2023-11-02 15:36:05 -0300

    feat: add Categories and CategoriesItem component

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,9 +1,23 @@
 "use client";
 
 import Image from "next/image";
+import Categories from "./components/categories";
 
 export default function Home() {
-  return <div className="p-5">
-    <Image src={"/banner-home-01.png"} height={0} width={0} className="h-auto w-full" sizes="100vw" alt="Até 55% de desconto esse Mês!"/>
-  </div>;
+  return (
+    <div className="p-5">
+      <Image
+        src={"/banner-home-01.png"}
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Até 55% de desconto esse Mês!"
+      />
+
+      <div className="mt-8">
+        <Categories />
+      </div>
+    </div>
+  );
 }
