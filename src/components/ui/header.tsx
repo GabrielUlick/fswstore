0486b01f3d07ec73d8d66ab2c680612b665c4c78
Gabriel Ulick <gabrieludb@hotmail.com>
2023-11-04 18:16:40 -0300

    feat: ensure user goes to catalog page

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -12,10 +12,17 @@ import {
 } from "lucide-react";
 import { Button } from "./button";
 import { Card, CardContent } from "./card";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import { Separator } from "./separator";
+import Link from "next/link";
 
 const Header = () => {
   const { status, data } = useSession();
@@ -48,8 +55,8 @@ const Header = () => {
                   {data.user.image && <AvatarImage src={data.user.image} />}
                 </Avatar>
                 <div className="flex flex-col">
-                <p className="font-medium">{data.user.name}</p>
-                <p className="text-sm opacity-75">Boas compras!</p>
+                  <p className="font-medium">{data.user.name}</p>
+                  <p className="text-sm opacity-75">Boas compras!</p>
                 </div>
               </div>
               <Separator />
@@ -85,10 +92,17 @@ const Header = () => {
               <PercentIcon size={16} />
               Ofertas
             </Button>
-            <Button variant={"outline"} className="w-full justify-start gap-2">
-              <ListOrderedIcon size={16} />
-              Catálogo
-            </Button>
+            <SheetClose asChild>
+              <Link href="/catalog">
+                <Button
+                  variant={"outline"}
+                  className="w-full justify-start gap-2"
+                >
+                  <ListOrderedIcon size={16} />
+                  Catálogo
+                </Button>
+              </Link>
+            </SheetClose>
           </div>
         </SheetContent>
       </Sheet>
