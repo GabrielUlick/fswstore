b9e48e57e5d2b9a16291fac501b2f20181fd9bdc
Gabriel Ulick <gabrieludb@hotmail.com>
2023-11-04 17:33:16 -0300

    feat: catalog page

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,6 +1,21 @@
-const CatalogPage = () => {
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react";
+import CategoryItem from "./components/category-item";
+
+const CatalogPage = async () => {
+    const categories = await prismaClient.category.findMany({});
     return (  
-        <h1>Catalog</h1>
+        <div className="flex flex-col p-5 gap-8">
+            <Badge className="w-fit gap-1 text-base uppercase border-2 border-primary px-3 py-1 py-[0.375rem]" variant="outline">
+                <ShapesIcon size={16}/>
+                Catálogo
+            </Badge>
+
+        <div className="grid grid-cols-2 gap-8">
+            {categories.map(category => <CategoryItem key={category.id} category={category}/>)}
+        </div>
+        </div>
     );
 }
  
